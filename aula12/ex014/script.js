b82d70514299eb49c036cd669692660d76145073
Marianne <marianne_msmelo@hotmail.com>
2022-02-24 15:14:26 -0300

    Update script.js

--- aula12/ex014/script.js
+++ aula12/ex014/script.js
@@ -3,7 +3,6 @@ var msg = window.document.getElementById("msg")
 var img = window.document.getElementById("imagem")
 var data = new Date()
 var hora = data.getHours()
-var hora = 
 msg.innerHTML = `Agora são ${hora} horas. `
 if(hora >= 0 && hora < 12){
     //BOM DIA
