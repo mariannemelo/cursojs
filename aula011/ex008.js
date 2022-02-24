8a12740738dd654950ef408d30a897d9b6421578
Marianne <marianne_msmelo@hotmail.com>
2022-02-24 15:10:05 -0300

    Initial commit

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 70
+console.log('a velocidade do carro é 70 km/h')
+if(vel > 60) {
+    console.log('você ultrapassou o limite de velocidade. Multado!')
+}
+console.log('Dirija com cuidado')
---
+++ aula12/ex014/script.js
@@ -0,0 +1,21 @@
+function carregar() {
+var msg = window.document.getElementById("msg")
+var img = window.document.getElementById("imagem")
+var data = new Date()
+var hora = data.getHours()
+var hora = 
+msg.innerHTML = `Agora são ${hora} horas. `
+if(hora >= 0 && hora < 12){
+    //BOM DIA
+    img.src = 'manha.png'
+    document.body.style.background = '#c4e7fd'
+} else if(hora >= 12 && hora < 18) {
+    //BOA TARDE
+    img.src = 'tarde.png'
+    document.body.style.background = '#d7bd9a'
+} else {
+    //BOA NOITE
+    img.src = 'noite.png'
+    document.body.style.background = '#1b2d37'
+}
+}
